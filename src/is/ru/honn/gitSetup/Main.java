1fa9b18f2d7103bef0a048267c78e6ebe9abebd8
Thordur Arnarson <thordurba08@ru.is>
2010-11-01 21:20:35 +0000

    First commit

---
+++ src/is/ru/honn/gitSetup/Main.java
@@ -0,0 +1,16 @@
+/**
+ * @(#)HelloGitWorld Nov 1, 2010 Thordur Arnarson
+ *
+ * Copyright (c) Thordur Arnarson
+ */
+package is.ru.honn.gitSetup;
+
+/**
+ * @author Thordur Arnarson
+ * @version 1.0 Nov 1, 2010
+ */
+public class Main {
+	public static void main(String args[]) {
+		System.out.println("Welcome to Git World!");
+	}
+}
